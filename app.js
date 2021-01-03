83621566038725cd1b823fa829c592027637654e
Wen Jen <wjen1214@gmail.com>
2021-01-03 14:58:48 -0800

    add movement functionality for tetrominoes

--- app.js
+++ app.js
@@ -66,5 +66,97 @@ document.addEventListener("DOMContentLoaded", () => {
       squares[currentPosition + index].classList.remove("tetromino");
     });
   }
-  draw();
+
+  // make the tetromino move down every second
+  timerId = setInterval(moveDown, 1000);
+
+  // assign function to keyCodes
+  function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    } else if (e.keyCode === 38) {
+      rotate();
+    } else if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 40) {
+      moveDown();
+    }
+  }
+
+  document.addEventListener("keyup", control);
+
+  // move down function
+  function moveDown() {
+    undraw();
+    currentPosition += GRID_WIDTH;
+    draw();
+    freeze();
+  }
+
+  function freeze() {
+    if (
+      current.some((index) =>
+        squares[currentPosition + index + GRID_WIDTH].classList.contains(
+          "taken"
+        )
+      )
+    ) {
+      current.forEach((index) =>
+        squares[currentPosition + index].classList.add("taken")
+      );
+      // start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
+  }
+
+  // move to the left unless at edge or there is a blockage
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(
+      (index) => (currentPosition + index) % GRID_WIDTH == 0
+    );
+    if (!isAtLeftEdge) {
+      currentPosition -= 1;
+    }
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition += 1;
+    }
+    draw();
+  }
+  // move to the right unless at edge or there is a blockage
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some(
+      (index) => (currentPosition + index) % GRID_WIDTH === GRID_WIDTH - 1
+    );
+    if (!isAtRightEdge) {
+      currentPosition += 1;
+    }
+    if (
+      current.some((index) => {
+        return squares[currentPosition + index].classList.contains("taken");
+      })
+    ) {
+      currentPosition -= 1;
+    }
+    draw();
+  }
+
+  // rotate tetrimino unless blocked
+  function rotate() {
+    undraw();
+    currentRotation++;
+    if (currentRotation === current.length) {
+      currentRotation = 0;
+    }
+    current = theTetrominoes[random][currentRotation];
+    draw();
+  }
 });
